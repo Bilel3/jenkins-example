d816b319b62075a403370d0f145c86ccf7e82aeb
Bilel3 <30528888+Bilel3@users.noreply.github.com>
2020-03-13 11:11:37 +0100

    Update testSSH2.sh

--- testSSH2.sh
+++ testSSH2.sh
@@ -1,12 +1,20 @@
+def remote = [:]
+remote.name = "DESKTOP-4AB5TSM"
+remote.host = '192.168.56.1'
+remote.allowAnyHosts = true
+
 node {
-  def remote = [:]
-  remote.name = 'DESKTOP-4AB5TSM'
-  remote.host = '192.168.56.1'
-  remote.user = 'bbayoudhi'
-  remote.password = 'Spark2020*-+'
-  remote.allowAnyHosts = true
-  stage('Remote SSH') {
-    sshCommand remote: remote, command: "ls -lrt"
-    sshCommand remote: remote, command: "for i in {1..5}; do echo -n \"Loop \$i \"; date ; sleep 1; done"
-  }
+    withCredentials([usernamePassword(credentialsId: 'bbayoudhi', passwordVariable: 'Spark2020*-+', usernameVariable: 'bbayoudhi')]) {
+        remote.user = bbayoudhi
+        remote.password = Spark2020*-+
+
+        stage("SSH Steps ") {
+            writeFile file: 'test.sh', text: 'ls'
+            sshCommand remote: remote, command: 'for i in {1..5}; do echo -n \"Loop \$i \"; date ; sleep 1; done'
+            sshScript remote: remote, script: 'test.sh'
+            sshPut remote: remote, from: 'test.sh', into: '.'
+            sshGet remote: remote, from: 'test.sh', into: 'test_new.sh', override: true
+            sshRemove remote: remote, path: 'test.sh'
+        }
+    }
 }
